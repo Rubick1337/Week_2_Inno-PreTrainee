64e514e071587c02422113c32cb8a663444bbbcb
Rubick1337 <poss1337@gmail.com>
2025-10-10 15:12:36 +0300

    feat: add migrations with table Tasks

---
+++ Week_2_Inno-PreTrainee/Infrastructure/Migrations/CreateTableTasks.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+
+namespace Week_2_Inno_PreTrainee.Infrastructure.Migrations
+
+{
+
+    [Migration(20251010)]
+
+    public class CreateTableTasks : Migration
+
+    {
+
+        public override void Up()
+
+        {
+            Create.Table("Tasks")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Title").AsString().NotNullable()
+                .WithColumn("Description").AsString().NotNullable()
+                .WithColumn("IsCompleted").AsBoolean().NotNullable().WithDefaultValue(false)
+                .WithColumn("CreatedAt").AsDateTime2().NotNullable().WithDefaultValue(DateTime.Now);
+        }
+
+        public override void Down()
+
+        {
+            Delete.Table("Tasks");
+        }
+
+    }
+
+}
---
+++ Week_2_Inno-PreTrainee/Infrastructure/Migrations/InsertTestData.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Week_2_Inno_PreTrainee.Infrastructure.Migrations
+{
+    [Migration(20251010)]
+    internal class InsertTestData: Migration
+    {
+     
+        public override void Up()
+        {
+            Insert.IntoTable("Tasks")
+                .Row(new { Titile = "Задание 1 неделя 1", Description = "Консольный кулькулятор", IsCompleted = true, CreatedAt = DateTime.Now })
+                .Row(new { Titile = "Задание 2 неделя 1", Description = "Асихронное приложение", IsCompleted = true, CreatedAt = DateTime.Now })
+                .Row(new { Titile = "Задание 3 неделя 2", Description = "Консольный кулькулятор", IsCompleted = false, CreatedAt = DateTime.Now });
+        }
+        public override void Down() 
+        {
+            Delete.FromTable("Tasks").AllRows();
+        }
+    }
+}
