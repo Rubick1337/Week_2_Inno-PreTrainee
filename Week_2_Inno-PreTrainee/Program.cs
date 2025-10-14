ff80cf77e9c1b03d799ee216e7f3124bc30e86ce
Rubick1337 <poss1337@gmail.com>
2025-10-15 00:02:27 +0300

    feat: add Dependencies in program.cs

--- Week_2_Inno-PreTrainee/Program.cs
+++ Week_2_Inno-PreTrainee/Program.cs
@@ -1,18 +1,42 @@
-﻿using Week_2_Inno_PreTrainee.Infrastructure.Data.Factories;
-using System.Data;
+﻿using Week_2_Inno_PreTrainee.Application.Services;
+using Week_2_Inno_PreTrainee.Application.UI;
 using Week_2_Inno_PreTrainee.Core.Factories;
 using Week_2_Inno_PreTrainee.Core.Interfaces;
+using Week_2_Inno_PreTrainee.Core.Services;
+using Week_2_Inno_PreTrainee.Infrastructure.Data.Factories;
+using Week_2_Inno_PreTrainee.Infrastructure.Migrations;
+using Week_2_Inno_PreTrainee.Infrastructure.Repository;
 
-namespace Week_2_Inno_PreTrainee
+class Program
 {
-    class Program
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
+        var connectionString = ConfigReader.GetConnectionString("SqlServer");
+
+        RunMigrations.ExecuteMigratuins(connectionString);
+
+        DatabaseConnectionFactory factory = new SqlServerFactory(connectionString);
+        IDataBaseConnection sqlServer = factory.CreateConnection();
+        sqlServer.Open();
+
+        try
         {
-            DatabaseConnectionFactory factory = new SqlServerFactory("Server=DESKTOP-30H8C5P;Database=Inno_Course;Trusted_Connection=true;TrustServerCertificate=true;");
-            IDataBaseConnection sqlServer = factory.CreateConnection();
-            Console.WriteLine(sqlServer.GetDatabaseType());
-            sqlServer.GetConnection().Open();
+            var dbConnection = sqlServer.GetConnection();
+            var taskRepository = new SqlServerTaskRepository(dbConnection);
+            var taskService = new TaskService(taskRepository);
+            var taskManager = new TaskManager(taskService);
+            var menuManager = new MenuManager(taskManager);
+
+            await menuManager.RunAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+            Console.ReadKey();
+        }
+        finally
+        {
+            sqlServer.Close();
         }
     }
 }
