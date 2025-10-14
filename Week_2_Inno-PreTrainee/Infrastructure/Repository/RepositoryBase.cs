f5b963b9480de4e21085f152b7c7609cecef44f9
Rubick1337 <poss1337@gmail.com>
2025-10-15 00:00:05 +0300

    feat: add repository add service

---
+++ Week_2_Inno-PreTrainee/Infrastructure/Repository/RepositoryBase.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week_2_Inno_PreTrainee.Core.Interfaces;
+
+namespace Week_2_Inno_PreTrainee.Infrastructure.Repository
+{
+    internal abstract class RepositoryBase<T> : IRepository<T> where T : class
+    {
+        protected readonly IDbConnection _connection;
+        protected readonly string _tableName;
+
+        public  RepositoryBase (IDbConnection connection, string tableName)
+        {
+            _connection = connection;
+            _tableName = tableName;
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var sql = $"SELECT * FROM {_tableName}";
+            return  await _connection.QueryAsync<T>(sql);
+        }
+        public abstract Task CreateAsync(T item);
+        public abstract Task UpdateStatusAsync(int id, bool isCompleted);
+
+        public async Task DeleteAsync(int id)
+        {
+            var sql = $"DELETE FROM {_tableName} WHERE Id = @id";
+            //var parametrs = new DynamicParameters();
+            //parametrs.Add("@id", id);
+            await _connection.ExecuteAsync(sql, new { id });
+        }
+        public void Dispose()
+        {
+            _connection?.Dispose();
+        }
+    }
+}
