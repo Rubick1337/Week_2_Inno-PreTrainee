08211d211fd41f205364c093f82e1c3e8111e5cc
Rubick1337 <poss1337@gmail.com>
2025-10-17 18:38:52 +0300

    feat: add class for Console

---
+++ Week_2_Inno-PreTrainee/Application/Services/OutputService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week_2_Inno_PreTrainee.Core.Interfaces;
+
+namespace Week_2_Inno_PreTrainee.Application.Services
+{
+    public class OutputService : IOutputService
+    {
+        static public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+       static public void WriteError(string message) 
+        { 
+            Console.WriteLine(message); 
+        }
+    }
+}
---
+++ Week_2_Inno-PreTrainee/Core/Interfaces/IOutputService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_Inno_PreTrainee.Core.Interfaces
+{
+    internal interface IOutputService
+    {
+        void WriteLine(string message);
+        void WriteError(string message);
+    }
+}
