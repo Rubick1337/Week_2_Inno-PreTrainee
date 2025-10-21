00a558aeb5fb715ccf2ca82ca55da4165cf6681b
Rubick1337 <poss1337@gmail.com>
2025-10-21 11:31:45 +0300

    feat: add ITaskService

---
+++ Week_2_Inno-PreTrainee/Application/Interfaces/ITaskService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskEntity = Week_2_Inno_PreTrainee.Core.Entities.Task;
+
+namespace Week_2_Inno_PreTrainee.Application.Interafaces
+{
+    public interface ITaskService
+    {
+        Task<IEnumerable<TaskEntity>> GetAllAsync();
+        Task CreateAsync(string title, string description);
+        Task UpdateStatusAsync(int id, bool isCompleted);
+        Task DeleteAsync(int id);
+    }
+}
